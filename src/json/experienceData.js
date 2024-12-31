9285ca8303f76c47e5916ac264e2027ef0e9e653
Junyu Pu <pujunyu@gmail.com>
2024-12-31 13:28:42 +0100

    fix: css styles and icons

--- src/json/experienceData.js
+++ src/json/experienceData.js
@@ -1,20 +1,20 @@
 export default [
     {
-        date: "2022 → Current",
+        date: "2022 ▸ Now",
         title: "Senior Product Manager",
         company: 'Delivery Hero SE',
         description: `Building a solution to allow entities across the global market to customize their 
         resolutions for customer contacts and enable self-service.`,
     },
     {
-        date: "2019 → 2022",
+        date: "2019 ▸ 2022",
         title: "Product Manager & Design Engineer",
         company: "LabTwin GmbH",
         description: `Worked as Design Engineer and transformed to Product Management.
         Built and created a voice assistant app to help scientists work efficiently in labs.`,
     },
     {
-        date: "2016 → 2019",
+        date: "2016 ▸ 2019",
         title: "Product Designer & Frontend Engineer",
         company: "3YOURMIND GmbH",
         description: `Designed and built a marketplace solution for 3D printings and 
